cbdcd69c93a686ac19e5f7d4d09b21bb3fba4e93
BlindBoko24 <57777366+RaSsaS-GD@users.noreply.github.com>
2023-01-10 21:32:56 -0600

    Add files via upload
    The number sorting system is ment to be run as a console application, but you can modify and delete all the unnecessary lines of code for it to work just as a function.

---
+++ NumberSorting.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace NumSortSystem
+{
+    class NumberSorting
+    {
+        static void Main()
+        {
+            Sort();
+        }
+
+        static void Sort()
+        {
+            int i;
+
+            Console.Write("Enter the Number of values to be Sort : ");
+
+            int n = Convert.ToInt16(Console.ReadLine());
+
+            int[] a = new int[n];
+
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("Enter the No " + i + ":");
+
+                a[i] = Convert.ToInt16(Console.ReadLine());
+            }
+
+            for (i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n - 1; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        int temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+
+            Console.Write("Ascending Sort : ");
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
